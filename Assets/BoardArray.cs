24e197d0f2df12715b17f06b4ac7308955830ee8
Ikaovret <tervo.aki@gmail.com>
2017-03-24 11:30:45 +0200

    test

---
+++ Assets/BoardArray.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardArray : MonoBehaviour {
+    private Vector3[,] locations = new Vector3[10,10];
+    private bool[,] isFree = new bool[10,10];
+	// Use this for initialization
+	void Awake ()
+    {
+        locations[0, 0] = GameObject.Find("A0").GetComponent<Transform>().position;
+        for(int i = 0; i<10; i++)
+        {
+            for(int j = 0; j<10; j++)
+            {
+                locations[i, j].x = locations[0, 0].x + 1;
+                locations[i, j].y = locations[0, 0].y;
+                locations[i, j].z = locations[0, 0].z;
+            }
+        }
+        for(int i = 0; i < 10; i++)
+        {
+            for(int j = 0; j<10; j++)
+            {
+                isFree[i, j] = false;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+	}
+
+    public Vector3[,] GetLocations()
+    {
+        return locations;
+    }
+
+    public bool[,] GetIsFree()
+    {
+        return isFree;
+    }
+
+    public void PlaceIsFree(int x, int y)
+    {
+        isFree[x, y] = true;
+    }
+
+    public void EmptyIsFree(int x, int y)
+    {
+        isFree[x, y] = false;
+    }
+}
